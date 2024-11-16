026a99c4a026d6eb435e34a0c8d18b3ab0d38dd3
unsubstantiated-Script <jusfulton@gmail.com>
2024-11-16 15:00:51 -0800

    Depth First Search

--- dataStructures/binarySearchTrees.js
+++ dataStructures/binarySearchTrees.js
@@ -56,6 +56,22 @@ class BinarySearchTree {
         return current;
     }
 
+    breadthFirstSearch() {
+        let data = [],
+            queue = [],
+            node = this.root;
+
+        queue.push(node);
+
+        while (queue.length) {
+            node = queue.shift();
+            data.push(node.value);
+            if(node.left) queue.push(node.left);
+            if(node.right) queue.push(node.right);
+        }
+        return data;
+    }
+
 }
 
 export {BinarySearchTree}
